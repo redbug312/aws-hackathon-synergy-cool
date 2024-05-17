1192d72d8224c40d81b8a0e03181f2019b4c577e
redbug312 <redbug312@gmail.com>
2024-05-18 01:29:10 +0800

    Respond bad request for missing parameters

--- src/lambda/detect_activities/app.py
+++ src/lambda/detect_activities/app.py
@@ -17,10 +17,22 @@ class Room:
         }
 
 
+def bad_request(message: str):
+    return {
+        'statusCode': 400,
+        'body': json.dumps({
+            'error': message,
+        }),
+    }
+
+
 def lambda_handler(event, context):
     path_parameters = event.get('pathParameters', {})
     room_id = path_parameters.get('room_id', None)
     logger.info(f'API queries the room {room_id}')
+
+    if room_id is None:
+        return bad_request('missing path parameter room_id')
 
     response = Room('diner', 0).to_dict()
     logger.info(f'API replies the room data {response}')
--- src/lambda/monitor_sensors/app.py
+++ src/lambda/monitor_sensors/app.py
@@ -17,10 +17,22 @@ class Sensor:
         }
 
 
+def bad_request(message: str):
+    return {
+        'statusCode': 400,
+        'body': json.dumps({
+            'error': message,
+        }),
+    }
+
+
 def lambda_handler(event, context):
     path_parameters = event.get('pathParameters', {})
     sensor_id = path_parameters.get('sensor_id', None)
     logger.info(f'API queries the sensor {sensor_id}')
+
+    if sensor_id is None:
+        return bad_request('missing path parameter session_id')
 
     response = Sensor(30.0, 50.0).to_dict()
     logger.info(f'API replies the sensor data {response}')
