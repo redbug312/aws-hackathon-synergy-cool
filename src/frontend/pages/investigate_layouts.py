f2a6beb4964a7ccdb565caa48004fc809ed98851
redbug312 <redbug312@gmail.com>
2024-05-18 23:21:59 +0800

    add-memory-to-chatbot

--- src/frontend/pages/investigate_layouts.py
+++ src/frontend/pages/investigate_layouts.py
@@ -1,20 +1,48 @@
 import streamlit as st
 from st_pages import add_page_title
 from langchain_aws import ChatBedrock
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import PromptTemplate
 
 add_page_title()
 
-llm = ChatBedrock(
-    model_id="anthropic.claude-v2:1",
-    model_kwargs={"temperature": 0},
-)
+template = """
+Human: The following is a friendly conversation between a human and an AI. The
+AI is talkative and provides lots of specific details from its context. If the
+AI does not know the answer to a question, it truthfully says it does not know.
 
-# Store LLM generated responses
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{
+Current conversation:
+<conversation_history>
+{history}
+</conversation_history>
+
+Here is the human's next reply:
+<human_reply>
+{input}
+</human_reply>
+
+AI:
+"""
+
+if "chain" not in st.session_state:
+    llm = ChatBedrock(
+        model_id="anthropic.claude-v2:1",
+        model_kwargs={"temperature": 0},
+    )
+    memory = ConversationBufferMemory(k=5)
+    prompt = PromptTemplate.from_template(template)
+    conversation = ConversationChain(
+        llm=llm, memory=memory, prompt=prompt, verbose=True
+    )
+    st.session_state.chain = conversation
+
+if "messages" not in st.session_state:
+    messages = [{
         "role": "assistant",
         "content": "How may I help you?"
     }]
+    st.session_state.messages = messages
 
 # Display chat messages
 for message in st.session_state.messages:
@@ -22,30 +50,19 @@ for message in st.session_state.messages:
         st.write(message["content"])
 
 
-# Function for generating LLM response
-def generate_response(prompt_input):
-    return llm.invoke(prompt_input)
-
-
 # User-provided prompt
-if prompt := st.chat_input("write here"):
-    st.session_state.messages.append({
-        "role": "user",
-        "content": prompt
-    })
-
+if question := st.chat_input("write here"):
     with st.chat_message("user"):
-        st.write(prompt)
+        st.write(question)
 
-    # Generate a new response if last message is not from assistant
-    if st.session_state.messages[-1]["role"] != "assistant":
-        with st.chat_message("assistant"):
-            with st.spinner("Thinking..."):
-                response = generate_response(prompt)
-                print(response)
-                st.write(response.content)
-        message = {
-            "role": "assistant",
-            "content": response.content
-        }
-        st.session_state.messages.append(message)
+    user_message = {"role": "user", "content": question}
+    st.session_state.messages.append(user_message)
+
+    with st.chat_message("assistant"):
+        conversation = st.session_state.chain
+        with st.spinner("Thinking..."):
+            response = conversation.predict(input=question)
+        st.write(response)
+
+    ai_message = {"role": "assistant", "content": response}
+    st.session_state.messages.append(ai_message)
