74bc5c0e696746f9a403c0029d87944fbf2bbf9b
redbug312 <redbug312@gmail.com>
2024-05-19 05:29:19 +0800

    Enhance chatbot with chat history

--- src/frontend/pages/investigate_layouts.py
+++ src/frontend/pages/investigate_layouts.py
@@ -3,6 +3,9 @@ from streamlit.logger import get_logger
 from st_pages import add_page_title
 
 from langchain_aws import ChatBedrock
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import PromptTemplate
 
 import logging
 
@@ -11,12 +14,34 @@ logger.setLevel(logging.DEBUG)
 
 add_page_title()
 
+
+template = """
+Human: The following is a friendly conversation between a human and an AI. The
+AI is talkative and provides lots of specific details from its context. If the
+AI does not know the answer to a question, it truthfully says it does not know.
+Current conversation:
+<conversation_history>
+{history}
+</conversation_history>
+Here is the human's next reply:
+<human_reply>
+{input}
+</human_reply>
+AI:
+"""
+
+
 if "chain" not in st.session_state:
     llm = ChatBedrock(
         model_id="anthropic.claude-v2:1",
         model_kwargs={"temperature": 0},
     )
-    st.session_state.chain = llm
+    memory = ConversationBufferMemory()
+    prompt = PromptTemplate.from_template(template)
+    conversation = ConversationChain(
+        llm=llm, memory=memory, prompt=prompt, verbose=True
+    )
+    st.session_state.chain = conversation
 
 if "messages" not in st.session_state:
     messages = [{
@@ -24,6 +49,7 @@ if "messages" not in st.session_state:
         "content": "How may I help you?"
     }]
     st.session_state.messages = messages
+
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
@@ -39,9 +65,9 @@ if question := st.chat_input("write here"):
     with st.chat_message("assistant"):
         conversation = st.session_state.chain
         with st.spinner("Thinking..."):
-            response = conversation.invoke(question)
+            response = conversation.predict(input=question)
             logger.debug(f"Obtained response {response} from chatbot")
-        st.write(response.content)
+        st.write(response)
 
-    ai_message = {"role": "assistant", "content": response.content}
+    ai_message = {"role": "assistant", "content": response}
     st.session_state.messages.append(ai_message)
